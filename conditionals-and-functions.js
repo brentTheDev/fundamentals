63ff128997518f821adda4cc1906d3fa2e2281cf
Brent Kie <brent.a.kie@gmail.com>
2018-03-20 14:33:53 -0600

    Fundamentals prework for DDC Fullstack bootcamp cohort 20

---
+++ conditionals-and-functions.js
@@ -0,0 +1,12 @@
+/*
+*Conditionals, Functions, Scope and Loops.
+*/
+
+let storeA = 33.10;
+let storeB = 4.40;
+
+let storeAIsLower = storeA < storeB;
+
+if (storeAIsLower) {
+  console.log("Store A has a lower price.")
+}
